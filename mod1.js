17b97fddd93cf057cd6a46efaf9fc49633834124
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2025-02-26 19:07:46 -0400

    Módulo 1...

---
+++ mod1.js
@@ -0,0 +1,13 @@
+const nome = "Miranda";
+const sobrenome = "Jorge";
+
+const falaNome = () => {
+    console.log(nome, sobrenome);
+}
+
+// module.exports.nome = nome
+
+exports.nome1 = nome
+this.qualquerCoisa = 'Eae, bão?';
+
+falaNome()
