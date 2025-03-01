1469fa9a7aeee35afd7010daf0136a5094bb5f90
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2025-03-01 19:29:04 -0400

    Aplicando estudos em express, criando servidores...

---
+++ modulo1/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+/*
+|-----------------------------------------------|
+|         criar   | ler   | atualizar | apagar  |     
+| CRUD -> CREATE, | READ, | UPDATE,   | DELETE  |
+|         POST,   | GET,  | PUT,      | DELETE  |     
+|-----------------------------------------------|
+*/
+
+app.get('/', (req, res) => {
+    res.send('Hello <b>World</b>');
+}); 
+
+app.get('/contato', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+        Nome: <input name type="text" name="nome">
+        <button>Enviar</button>
+    </form>    
+    `)
+})
+
+app.post('/', (req, res) => {
+    res.send('O formulário foi enviado com sucesso.')
+})
+
+app.listen(3000, () => {
+    console.log('Servidor executando - port: 3000');
+    console.log('Acesse >> http://localhost:3000');
+});
