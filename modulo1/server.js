eabf82ebc5d6e12740a98779a2d8dcdf1e710a86
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2025-03-04 20:38:03 -0400

    Express - req.params, req.query, req.body. Entendendo cada um e onde se aplicam...

--- modulo1/server.js
+++ modulo1/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// express.urlencoded({ extended: true }) serve para interpretar os dados enviados através de um formulário HTML com method="POST", convertendo-os em um objeto JavaScript acessível via req.body.
+app.use(express.urlencoded({ extended: true }))
+
 /*
 |-----------------------------------------------|
 |         criar   | ler   | atualizar | apagar  |     
@@ -10,20 +13,28 @@ const app = express();
 */
 
 app.get('/', (req, res) => {
-    res.send('Hello <b>World</b>');
-}); 
-
-app.get('/contato', (req, res) => {
     res.send(`
     <form action="/" method="POST">
-        Nome do cliente: <input name type="text" name="nome">
+        Nome do cliente: <input type="text" name="nome">
         <button>Enviar</button>
     </form>    
     `)
 })
 
+// O ":" indica que é um parâmetro, 
+// e o "?" indica que o parâmetro é opcional
+app.get('/testes/:profileName?/:profileSenha?', (req, res) => {
+    // params - Usado para capturar valores definidos na própria URL
+    console.log(req.params);
+    // query - Usado para capturados enviados na URL depois do ?
+    console.log(req.query);
+    res.send([req.params, req.query])
+})
+
 app.post('/', (req, res) => {
-    res.send('O formulário foi enviado com sucesso.')
+    // body - Usado para capturar dados enviados no corpo da requisição, geralmente em POST, PUT ou PATCH.
+    console.log(req.body);
+    res.send(`O formulário foi enviado com sucesso. Oque você enviou foi: ${req.body.nome}` )
 })
 
 app.listen(3000, () => {
