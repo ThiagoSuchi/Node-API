48f474bd4d5591c778afd05b279932967aab4c75
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2025-03-08 15:31:10 -0400

    Fazendo a primeira conexão com o MongoDB Atlas e primeiro Model...

--- modulo2/server2.js
+++ modulo2/server2.js
@@ -1,5 +1,18 @@
+// Importando a conexão do mongoDB Atlas, que está no arquivo .env
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Conectando ao Banco de Dados Atlas
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    console.log('>> Conectado ao Banco de Dados Atlas <<')
+    app.emit('pronto')// Emite um evento chamado 'pronto'
+  })
+  .catch(err => console.error(err));
+
 const routes = require('./routes.js');
 const path = require('path');
 const meuMiddleware = require('./src/middlewares/middleware.js')
@@ -15,6 +28,8 @@ app.set('view engine', 'ejs');
 app.use(meuMiddleware);
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Acessar >> http://localhost:3000');
-});
+app.on('pronto', () => {// Captura o evento 'pronto', e inicia a função.
+    app.listen(3000, () => {
+        console.log('Acessar => http://localhost:3000');
+    });
+})
