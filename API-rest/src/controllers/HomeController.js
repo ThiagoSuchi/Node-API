9cdc666bf97ba5f1503e772a761b70d016e4e881
ThiagoHS <thiagohenssuchi@gmail.com>
2025-03-21 19:45:03 -0400

    Configuring and testing sequelize, tabloe structure created in the database...

--- API-rest/src/controllers/HomeController.js
+++ API-rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from "../models/alunoModel";
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      TudoCerto: true,
-    });
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'George',
+      sobrenome: 'Leoncio',
+      email: 'leleONCIO@gmail.com',
+      idade: 26,
+      peso: 83,
+      altura: 1.86,
+    })
+    res.json(novoAluno);
   }
 }
 
