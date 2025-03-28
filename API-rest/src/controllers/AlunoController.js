45295c4baaeb9df6816790971171e0b52cfdbb5e
ThiagoHS <thiagohenssuchi@gmail.com>
2025-03-28 17:36:59 -0400

    Students CRUD...

--- API-rest/src/controllers/AlunoController.js
+++ API-rest/src/controllers/AlunoController.js
@@ -5,6 +5,102 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos );
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      })
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if(!id) {
+        return res.status(400).json({
+          errors: ['faltando ID'],
+        })
+      };
+
+      const aluno = await Aluno.findByPk(id);
+
+      if(!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não foi encontrado.'],
+        })
+      };
+
+      return res.json(aluno);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.mesage),
+      })
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if(!id) {
+        return res.status(400).json({
+          errors: ['faltando ID'],
+        })
+      };
+
+      const aluno = await Aluno.findByPk(id);
+
+      if(!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não foi encontrado.'],
+        })
+      };
+
+      await aluno.destroy()
+      return res.json({
+        apagado: true,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.mesage),
+      })
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if(!id) {
+        return res.status(400).json({
+          errors: ['faltando ID'],
+        })
+      };
+
+      const aluno = await Aluno.findByPk(id);
+
+      if(!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não foi encontrado.'],
+        })
+      };
+
+      const alunoAtualizado = await aluno.update(req.body);
+
+      return res.json(alunoAtualizado);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.mesage),
+      })
+    }
+  }
+
 };
 
 export default new AlunoController();
--- API-rest/src/middlewares/lognRequired.js
+++ API-rest/src/middlewares/lognRequired.js
@@ -1,42 +1,47 @@
 import jwt from "jsonwebtoken";
 import User from "../models/UserModel.js";
 
+// Middleware para verificar se o usuário está logado
 export default async (req, res, next) => {
-  const { authorization } = req.headers;
+  const { authorization } = req.headers; // Extrai o cabeçalho de autorização da requisição
 
+  // Se nenhum cabeçalho de autorização estiver presente, retorna um erro
   if (!authorization) {
     return res.status(401).json({
       errors: ['Login required'],
-    })
+    });
   }
 
+  // Extrai o token do cabeçalho de autorização
   const [, token] = authorization.split(' ');
 
   try {
-    const dados = jwt.verify(token, process.env.TOKEN_SECRET);
-    const { id, email } = dados;
+    // Verifica o token e extrai os dados do usuário
+    const dados = jwt.verify(token, process.env.TOKEN_SECRET); // Valida o token usando a chave secreta
+    const { id, email } = dados; // Desestrutura o ID e o email do usuário do payload do token
 
     const user = await User.findOne({
       where: {
         id,
         email,
-      }
+      },
     });
 
     if (!user) {
       return res.status(401).json({
-        errors: ['Usuário inválido.']
-      })
+        errors: ['Usuário inválido.'],
+      });
     }
 
+    // Anexa as informações do usuário ao objeto da requisição para uso posterior
     req.userId = id;
     req.userEmail = email;
-    return next()
+    return next();
 
   } catch (err) {
     console.log(err);
     return res.status(401).json({
       errors: ['Token expirado ou inválido.'],
-    })
+    });
   }
-}
+};
