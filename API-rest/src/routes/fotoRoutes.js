194ebe299f99238f43912dc110a6492f5647e563
ThiagoHS <thiagohenssuchi@gmail.com>
2025-03-31 16:09:26 -0400

    Handling uploaded files - handling erros...

--- API-rest/src/routes/fotoRoutes.js
+++ API-rest/src/routes/fotoRoutes.js
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import multer from "multer";
 
 import fotoController from "../controllers/FotoController.js"
-import multerConfig from "../config/multerConfig.js";
 
-const upload = multer(multerConfig);
 const router = new Router();
 
-router.post('/',upload.single('imagem'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
