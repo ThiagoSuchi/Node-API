4d381a45f3143bfb093b25b34cd3a330153dc016
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-07 15:52:58 -0400

    Add errors middleware...

--- API-REST-Udemy/myApi/src/shared/http/routes/index.ts
+++ API-REST-Udemy/myApi/src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
+import { AppError } from "@shared/errors/AppErro";
 import { Router } from "express"; //permite criar grupos de rotas em arquivos separados e, então, importar essas rotas para o arquivo principal
 
-const routes = Router()
+const routes = Router();
 
 routes.get('/', (req, res) => {
+  throw new AppError("Acesso negado.")
   res.json({ message: 'Hello World!!' });
 });
 
-export { routes }
+export { routes };
--- API-REST-Udemy/myApi/src/shared/http/server.ts
+++ API-REST-Udemy/myApi/src/shared/http/server.ts
@@ -1,15 +1,38 @@
 import 'dotenv/config'
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { routes } from './routes';
 import 'express-async-errors';
 import cors from 'cors';
+import { AppError } from '@shared/errors/AppErro';
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(routes)
 app.use(cors());
 app.use(express.json());
+
+app.use(routes)
+
+app.use(((((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  console.log(error);
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.',
+  });
+}) as unknown) as express.ErrorRequestHandler));
+/*
+Oque o middleware acima faz:
+ - Captura erros lançados por throw ou next(error) em qualquer parte da sua aplicação;
+ - Responde de forma apropriada, dependendo se é um erro conhecido (AppError) ou não;
+ - Silencia o erro de tipagem do TypeScript com as unknown as express.ErrorRequestHandler.
+*/
 
 app.listen(port, () => {
   console.log(`Server started on port http://localhost:${port}/`);
