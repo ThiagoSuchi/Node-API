07926dd6b630526567f69902ecac6caae935349f
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-14 17:12:05 -0400

    Create findByName method...

--- API-REST-Udemy/myApi/src/roles/http/routes/roles.routes.ts
+++ API-REST-Udemy/myApi/src/roles/http/routes/roles.routes.ts
@@ -7,6 +7,12 @@ const rolesRepository = new RolesRepository();
 
 rolesRouter.post("/", (req, res) => {
   const { name } = req.body;
+  const roleAlreadyExists = rolesRepository.findByName(name);
+
+  if (roleAlreadyExists) {
+    res.status(400).json({ error: 'Role already exists.' })
+  }
+
   const role = rolesRepository.create({ name });
 
   res.status(200).json({ role });
