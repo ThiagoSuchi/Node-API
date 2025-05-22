766e187f2967ea7b1ab8425c642ed5d2ba41649c
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-22 18:16:57 -0400

    Testando varias class de um arquivo - Com toBeClosedTo...

---
+++ API-REST-Udemy/introducao/src/classes/discount.spec.ts
@@ -0,0 +1,34 @@
+import {
+    Discount,
+    FiftyPercentDiscount,
+    NoDiscount,
+    TenPercentDiscount
+} from "./discount";
+
+const createSut = (className: new () => Discount): Discount => {
+    return new className();
+}
+
+describe('Product', () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it('should have no discount', () => {
+        const sut = createSut(NoDiscount);
+        expect(sut.calculate(10.8)).toBeCloseTo(10.8);
+    });
+
+    it('should have no discount', () => {
+        const sut = createSut(NoDiscount);
+        expect(sut.calculate(10.8)).toBeCloseTo(10.8);
+    });
+
+    it('should apply 50% discount on price', () => {
+        const sut = createSut(FiftyPercentDiscount);
+        expect(sut.calculate(150.50)).toBeCloseTo(75.25);
+    });
+
+    it('should apply 10% discount on price', () => {
+        const sut = createSut(TenPercentDiscount);
+        expect(sut.calculate(10)).toBeCloseTo(9);
+    });
+});
