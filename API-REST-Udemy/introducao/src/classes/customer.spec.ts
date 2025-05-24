fb2866b3a800efa671ee2327faff4accff6bc9f5
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-24 18:44:26 -0400

    Testando customers...

--- API-REST-Udemy/introducao/src/classes/customer.spec.ts
+++ API-REST-Udemy/introducao/src/classes/customer.spec.ts
@@ -32,17 +32,16 @@ describe('IndividualCustomer', () => {
     });
 });
 
-describe('EnterpriseC', () => {
-    it('should have firstName, lastName and cpf', () => {
-        const sut = createIndividualCustomer('Larissa', 'Da Silva', '765-832.156.54');
-        expect(sut).toHaveProperty('firstName', 'Larissa');
-        expect(sut).toHaveProperty('lastName', 'Da Silva');
-        expect(sut).toHaveProperty('cpf', '765-832.156.54');
+describe('EnterpriseCustomer', () => {
+    it('should have name and cnpj', () => {
+        const sut = createEnterpriseCustomer('Udemy', '323');
+        expect(sut).toHaveProperty('name', 'Udemy');
+        expect(sut).toHaveProperty('cnpj', '323');
     });
 
-    it('should have methods to get name and idn', () => {
-        const sut = createIndividualCustomer('Larissa', 'Da Silva', '765-832.156.54');
-        expect(sut.getName()).toBe('Larissa Da Silva');
-        expect(sut.getIDN()).toBe('765-832.156.54');
+    it('should have methods to get name and idn for enterprise customers', () => {
+        const sut = createEnterpriseCustomer('Udemy', '222');
+        expect(sut.getName()).toBe('Udemy');
+        expect(sut.getIDN()).toBe('222');
     });
 });
