c956497de71d201659e76e10701079b2ad493542
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-29 17:30:17 -0400

    Create migration CreateRolesTable - Implementing typeOrm and SQLite3...

--- API-REST-Udemy/myApi/src/shared/typeorm/index.ts
+++ API-REST-Udemy/myApi/src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm"
+import { CreateRolesTable1748552621145 } from "./migrations/1748552621145-CreateRolesTable"
 
 // Configuração principal com o database
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./src/database/db.sqlite",
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1748552621145],
 })
---
+++ API-REST-Udemy/myApi/src/shared/typeorm/migrations/1748552621145-CreateRolesTable.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+// Migração no TypeORM é um jeito de registrar e aplicar mudanças no banco com segurança, versão e organização.
+export class CreateRolesTable1748552621145 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'roles',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'string',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          }
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('roles')
+  }
+}
+/* Comandos comuns no TypeORM:
+typeorm migration:create	Cria uma nova migration vazia
+typeorm migration:generate	Gera automaticamente com base nas entidades
+typeorm migration:run	Aplica todas as migrations pendentes
+typeorm migration:revert	Desfaz a última migration aplicada
+*/
