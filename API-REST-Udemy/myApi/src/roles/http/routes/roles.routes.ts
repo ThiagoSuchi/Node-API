45be9f86d17f23b4aa292062fb39138e353eb929
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-28 20:07:40 -0400

    Create useCase listRoles

--- API-REST-Udemy/myApi/src/roles/http/routes/roles.routes.ts
+++ API-REST-Udemy/myApi/src/roles/http/routes/roles.routes.ts
@@ -1,18 +1,19 @@
 //permite criar grupos de rotas em arquivos separados e, então, importar essas rotas para o arquivo principal
 import { RolesRepository } from "@roles/repositories/RolesRepository";
 import { createRolesController } from "@roles/useCases/createRole";
+import { listRolesController } from "@roles/useCases/listRoles";
 import { Router } from "express";
 
 const rolesRouter = Router();
-const rolesRepository = new RolesRepository();
 
+// Cria uma nova role
 rolesRouter.post("/", (req, res) => {
   createRolesController.handle(req, res)
 });
 
+// Gera uma lista com todas as roles
 rolesRouter.get("/", (req, res) => {
-  const roles = rolesRepository.findAll();
-  res.json(roles);
+  listRolesController.handle(req, res);
 });
 
 export { rolesRouter };
---
+++ API-REST-Udemy/myApi/src/roles/useCases/listRoles/index.ts
@@ -0,0 +1,8 @@
+import { RolesRepository } from "@roles/repositories/RolesRepository";
+import { ListRolesUseCase } from "./ListRolesUseCase";
+import { ListRolesController } from "./ListRolesController";
+
+
+const roleRepository = new RolesRepository();
+const listRolesUseCase = new ListRolesUseCase(roleRepository);
+export const listRolesController = new ListRolesController(listRolesUseCase);
