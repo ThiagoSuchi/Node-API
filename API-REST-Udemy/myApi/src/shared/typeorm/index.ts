c70f1eb08fd3772e75c61a6282c0c09c67697d8d
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-29 16:45:56 -0400

    Config database connection

--- API-REST-Udemy/myApi/src/shared/typeorm/index.ts
+++ API-REST-Udemy/myApi/src/shared/typeorm/index.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm"
 // Configuração principal com o database
 export const dataSource = new DataSource({
   type: "sqlite",
-  database: "./db.sqlite",
+  database: "./src/database/db.sqlite",
   entities: [],
   migrations: [],
 })
