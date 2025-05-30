274e95fe916d2526e753dd396c053b6dd53754c5
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-30 16:49:24 -0400

    Refactor role entity

--- API-REST-Udemy/myApi/src/shared/typeorm/index.ts
+++ API-REST-Udemy/myApi/src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { DataSource } from "typeorm"
 import { CreateRolesTable1748552621145 } from "./migrations/1748552621145-CreateRolesTable"
+import { Role } from "@roles/entities/Role"
 
 // Configuração principal com o database
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./src/database/db.sqlite",
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1748552621145],
 })
