fbfe43a6d3c297e64facb7247b04fde67b4d76b0
ThiagoHS <thiagohenssuchi@gmail.com>
2025-06-24 10:54:52 -0400

    Create user entity...

--- API-REST-Udemy/myApi/src/shared/typeorm/index.ts
+++ API-REST-Udemy/myApi/src/shared/typeorm/index.ts
@@ -3,12 +3,13 @@ import { CreateRolesTable1748552621145 } from "./migrations/1748552621145-Create
 import { Role } from "@roles/entities/Role"
 import { CreateUsersTable1750281006168 } from "./migrations/1750281006168-CreateUsersTable"
 import { AddRoleIdToUsersTable1750775931508 } from "./migrations/1750775931508-AddRoleIdToUsersTable"
+import { User } from "@users/entities/User"
 
 // Configuração principal com o database
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./src/database/db.sqlite",
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1748552621145,
     CreateUsersTable1750281006168,
