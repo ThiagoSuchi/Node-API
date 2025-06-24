400c66e6a5bab1ce16717a7f8a7f8a07d09bc5b6
ThiagoHS <thiagohenssuchi@gmail.com>
2025-06-24 10:51:00 -0400

    Create migration AddRoleIdToUsersTable

--- API-REST-Udemy/myApi/src/shared/typeorm/index.ts
+++ API-REST-Udemy/myApi/src/shared/typeorm/index.ts
@@ -2,11 +2,16 @@ import { DataSource } from "typeorm"
 import { CreateRolesTable1748552621145 } from "./migrations/1748552621145-CreateRolesTable"
 import { Role } from "@roles/entities/Role"
 import { CreateUsersTable1750281006168 } from "./migrations/1750281006168-CreateUsersTable"
+import { AddRoleIdToUsersTable1750775931508 } from "./migrations/1750775931508-AddRoleIdToUsersTable"
 
 // Configuração principal com o database
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./src/database/db.sqlite",
   entities: [Role],
-  migrations: [CreateRolesTable1748552621145, CreateUsersTable1750281006168],
+  migrations: [
+    CreateRolesTable1748552621145,
+    CreateUsersTable1750281006168,
+    AddRoleIdToUsersTable1750775931508
+  ],
 })
