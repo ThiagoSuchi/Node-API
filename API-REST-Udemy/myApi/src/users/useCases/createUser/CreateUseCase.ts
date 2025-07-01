f723e4a686f3046c5c8bcb972176ef355632a586
ThiagoHS <thiagohenssuchi@gmail.com>
2025-07-01 16:36:41 -0400

    Add CreateUserUseCase and installing bcrypt to hash passwords

---
+++ API-REST-Udemy/myApi/src/users/useCases/createUser/CreateUseCase.ts
@@ -0,0 +1,54 @@
+import { IRolesRepository } from "@roles/repositories/IRolesRepository";
+import { AppError } from "@shared/utils/errors/AppErro";
+import { User } from "@users/entities/User";
+import { IUserRepository } from "@users/repositories/IUserRepository";
+
+import { hash } from "bcryptjs";
+import { inject, injectable } from "tsyringe";
+
+export type CreateUserDTO = {
+  name: string
+  email: string
+  password: string
+  isAdmin: boolean
+  roleId: string
+}
+
+@injectable()
+export class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository') private usersRepository: IUserRepository,
+    @inject('RolesRepository') private rolesRepository: IRolesRepository,
+  ) {}
+
+  async execute({
+    name,
+    email,
+    isAdmin,
+    password,
+    roleId
+  }: CreateUserDTO): Promise<User> {
+    const emailExists = await this.usersRepository.findByEmail(email)
+
+    if (emailExists) {
+      throw new AppError('Email address already used.')
+    }
+
+    const role = await this.rolesRepository.findById(roleId)
+
+    if (!role) {
+      throw new AppError('Role not found.', 404)
+    }
+
+    const hashedPassword = await hash(password, 10)
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+      isAdmin,
+      role,
+    })
+
+    return user
+  }
+}
