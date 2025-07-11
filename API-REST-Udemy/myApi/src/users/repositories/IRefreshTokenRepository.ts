fd38b43f3057c6dcbc0de89cd7033c5a5700003a
ThiagoHS <thiagohenssuchi@gmail.com>
2025-07-11 14:29:37 -0400

    Create RefreshToken repository interface

---
+++ API-REST-Udemy/myApi/src/users/repositories/IRefreshTokenRepository.ts
@@ -0,0 +1,14 @@
+import { RefreshToken } from "@users/entities/RefreshToken.ts";
+
+type CreateRefreshTokenDTO = {
+  user_id: string;
+  token: string;
+  expires: Date;
+  valid: boolean;
+}
+
+export interface IRefreshTokenRepository {
+  create({ user_id, token, expires, valid }: CreateRefreshTokenDTO): Promise<RefreshToken>
+  findByToken(token: string): Promise<RefreshToken | null>
+  invalidate(refreshToken: RefreshToken): Promise<void>
+}
